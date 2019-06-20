b4e5f29b499522f2c28563d0b6a07c8f41a368b9
YouJia Shhi <shi.shiyoujia@gmail.com>
2019-06-19 18:41:22 -0700

    initial

---
+++ src/robots.js
@@ -0,0 +1,34 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jin Yang',
+        username: 'Gin',
+        email: 'jin@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Xiao Mei Yang',
+        username: 'Mei',
+        email: 'Mei@gmail.com'
+    },    {
+        id: 3,
+        name: 'YouJia Shi',
+        username: 'Jia',
+        email: 'jia@gmail.com'
+    },    {
+        id: 4,
+        name: 'Dalena Tran',
+        username: 'Dalena',
+        email: 'cremesaver@gmail.com'
+    },    {
+        id: 5,
+        name: 'Jennifer Nguyen',
+        username: 'Jen',
+        email: 'jen@gmail.com'
+    },    {
+        id: 6,
+        name: 'Mikey Su',
+        username: 'Mike',
+        email: 'Su@gmail.com'
+    },
+]
